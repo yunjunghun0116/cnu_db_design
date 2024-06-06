16bc837b095fec6a3afc74e636dba77263c0e66a
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-06-06 21:50:40 +0900

    feat: 이용자 회원가입 (#1)
    비밀번호를 암호화 한 후 해당 비밀번호를 저장하는 register 함수를 개발

---
+++ src/main/java/com/junghun/con/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.junghun.con.domain.user.repository;
+
+import com.junghun.con.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/junghun/con/util/RegexUtils.java
@@ -0,0 +1,23 @@
+package com.junghun.con.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class RegexUtils {
+    private static final String EMAIL_REGEX = "[0-9a-z]([\\-.\\w]*[0-9a-z\\-_+])*@([0-9a-z][\\-\\w]*[0-9a-z]\\.)+[a-z]{2,9}";
+    private static final String PASSWORD_REGEX = "[0-9a-zA-Z]{8,}";
+    private static final String NAME_REGEX = "[ㄱ-ㅎ가-힣a-zA-Z]{2,8}";
+    public static boolean isValidEmail(String email){
+        return Pattern.matches(EMAIL_REGEX,email);
+    }
+
+    public static boolean isValidPassword(String password){
+        return Pattern.matches(PASSWORD_REGEX,password);
+    }
+    public static boolean isValidName(String name){
+        return Pattern.matches(NAME_REGEX,name);
+    }
+}
