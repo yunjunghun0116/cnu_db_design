3f78bb299d3e04823c76039cac9fc286bdddda49
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-14 10:38:44 +0900

    remove: webapp폴더(JSP 반환) 제거
    기존 .jsp를 제공하기로 했던 부분을 프론트와 백 나누어서 개발 후 제공하기로 함
    그렇기에 webapp폴더가 불필요해져서 제거하기로함

--- src/main/java/com/junghun/con/ConApplication.java
+++ src/main/java/com/junghun/con/ConApplication.java
@@ -7,15 +7,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class ConApplication {
-
-	@Bean
-	public InternalResourceViewResolver viewResolver(){
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConApplication.class, args);
 	}
